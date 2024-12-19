dfb8a957b25b6a4423af87c902df3ae8c682dc89
sandy <sandyidn04@gmail.com>
2024-12-19 16:12:29 +0700

    modify buy and add search, sort on admin

--- main-program/main.c
+++ main-program/main.c
@@ -30,7 +30,7 @@ User *user = NULL;
 int hitungUser = 0;
 int kapasitasUser = 2;
 int hitungAntrian = 0;
-char antrian[MAX_QUEUE][MAX_QUEUE];
+// char antrian[MAX_QUEUE][MAX_QUEUE];
 User *userSaatIni = NULL;
 int hitungHistory = 0;
 
@@ -64,9 +64,48 @@ Transaksi history[MAX_HISTORY];
 
 typedef struct {
     User antrian[MAX_QUEUE];
-    int front;
-    int rear;
+    int front, rear;
 } Queue;
+
+Queue queue;
+
+void initQueue() {
+    queue.front = -1;
+    queue.rear = -1;
+}
+
+int isQueueFull() {
+    return (queue.rear + 1) % MAX_QUEUE == queue.front;
+}
+
+int isQueueEmpty() {
+    return queue.front == -1;
+}
+
+void enqueue(User user) {
+    if (isQueueFull()) {
+        printf("Antrian penuh. Tidak dapat menambahkan %s.\n", user.nama);
+        return;
+    }
+    if (isQueueEmpty()) {
+        queue.front = 0;
+    }
+    queue.rear = (queue.rear + 1) % MAX_QUEUE;
+    queue.antrian[queue.rear] = user;
+    printf("%s telah ditambahkan ke antrian.\n", user.nama);
+}
+
+void dequeue() {
+    if (isQueueEmpty()) {
+        printf("Antrian kosong. Tidak ada yang dapat dihapus.\n");
+        return;
+    }
+    printf("%s telah dihapus dari antrian.\n", queue.antrian[queue.front].nama);
+    if (queue.front == queue.rear) {
+        queue.front = -1;
+        queue.rear = -1;
+    } else queue.front = (queue.front + 1) % MAX_QUEUE;
+}
 
 void alokasiMemoriAwal() {
     buku = (Buku *)malloc(kapasitasBuku * sizeof(Buku));
@@ -244,6 +283,53 @@ void tambahBuku() {
     printf("Buku berhasil ditambahkan!\n");
 }
 
+void linearSearch(const char *key, const char *value) {
+    int found = 0;
+
+    for (int i = 0; i < hitungBuku; i++) {
+        if ((strcmp(key, "judul") == 0 && strcasecmp(buku[i].judul, value) == 0) ||
+            (strcmp(key, "pengarang") == 0 && strcasecmp(buku[i].pengarang, value) == 0) ||
+            (strcmp(key, "kategori") == 0 && strcasecmp(buku[i].kategori, value) == 0)) {
+            printf("\nBuku ditemukan:\n");
+            printf("Judul: %s\nPengarang: %s\nTahun Terbit: %d\nKategori: %s\nHarga: %.2f\nStok: %d\nStatus: %s\n",
+                   buku[i].judul, buku[i].pengarang, buku[i].tahun_terbit, buku[i].kategori, buku[i].harga, buku[i].stok, buku[i].status);
+            found = 1;
+        }
+    }
+
+    if (!found) {
+        printf("\nBuku dengan %s '%s' tidak ditemukan.\n", key, value);
+    }
+}
+
+void sortTahunTerbit(int descending) {
+    for (int i = 1; i < hitungBuku; i++) {
+        Buku key = buku[i];
+        int j = i - 1;
+
+        while (j >= 0 && ((descending && buku[j].tahun_terbit < key.tahun_terbit) ||
+                          (!descending && buku[j].tahun_terbit > key.tahun_terbit))) {
+            buku[j + 1] = buku[j];
+            j--;
+        }
+        buku[j + 1] = key;
+    }
+}
+
+void sortHarga(int ascending) {
+    for (int i = 1; i < hitungBuku; i++) {
+        Buku key = buku[i];
+        int j = i - 1;
+
+        while (j >= 0 && ((ascending && buku[j].harga > key.harga) ||
+                          (!ascending && buku[j].harga < key.harga))) {
+            buku[j + 1] = buku[j];
+            j--;
+        }
+        buku[j + 1] = key;
+    }
+}
+
 void deleteBuku() {
     char judul[100];
     printf("Masukkan judul buku yang ingin dihapus: ");
@@ -306,21 +392,28 @@ void updateStatusBuku() {
 }
 
 void displayQueue() {
+    if (isQueueEmpty()) {
+        printf("Antrian kosong.\n");
+        return;
+    }
     printf("\nAntrian Preorder:\n");
-    for (int i = 0; i < hitungAntrian; i++) {
-        printf("%d. %s\n", i + 1, antrian[i]);
+    int i = queue.front;
+    while (1) {
+        printf("%s\n", queue.antrian[i].nama);
+        if (i == queue.rear) break;
+        i = (i + 1) % MAX_QUEUE;
     }
 }
 
-void addToQueue(const char *judul) {
-    if (hitungAntrian >= MAX_QUEUE) {
-        printf("Antrian penuh.\n");
-        return;
-    }
-    strcpy(antrian[hitungAntrian], judul);
-    hitungAntrian++;
-    printf("Buku %s telah ditambahkan ke antrian.\n", judul);
-}
+// void addToQueue(const char *judul) {
+//     if (hitungAntrian >= MAX_QUEUE) {
+//         printf("Antrian penuh.\n");
+//         return;
+//     }
+//     strcpy(antrian[hitungAntrian], judul);
+//     hitungAntrian++;
+//     printf("Buku %s telah ditambahkan ke antrian.\n", judul);
+// }
 
 void beliBuku() {
     char judul[100];
@@ -332,11 +425,73 @@ void beliBuku() {
     for (int i = 0; i < hitungBuku; i++) {
         if (strcmp(buku[i].judul, judul) == 0) {
             if (strcmp(buku[i].status, "preorder") == 0) {
-                addToQueue(judul);
+                buku[i].stok = 0;
+                if (userSaatIni != NULL) {
+                    enqueue(*userSaatIni);
+                }
                 printf("Buku dalam status pre-order. Buku dengan judul %s berhasil ditambahkan ke list preorder.\n", judul);
             } else if (buku[i].stok > 0) {
                 buku[i].stok--;
-                printf("Pembelian buku %s berhasil!\n", judul);
+                int jumlah;
+                printf("Masukkan jumlah buku yang ingin dibeli: ");
+                scanf("%d", &jumlah);
+
+                float total = buku[i].harga * jumlah;
+                printf("Total harga: %.2f\n", total);
+
+                int metode;
+                Transaksi transaksi;
+                transaksi.id_transaksi = rand() % 10000;
+                strcpy(transaksi.judul, judul);
+                transaksi.jumlah = jumlah;
+                transaksi.total = total;
+                
+                printf("Pilih metode pembayaran:\n1. Kartu kredit\n2. Dompet digital\n3. Transfer bank\n");
+                printf("Pilih metode: ");
+                scanf("%d", &metode);
+                getchar();
+
+                if (metode == 1) {
+                    strcpy(transaksi.metode, "Kartu Kredit");
+                    printf("Masukkan nomor kartu kredit: ");
+                    fgets(transaksi.pembayaran.kredit.nomor_kartu, 20, stdin);
+                    removeNewline(transaksi.pembayaran.kredit.nomor_kartu);
+
+                    printf("Masukkan nama pemilik kartu: ");
+                    fgets(transaksi.pembayaran.kredit.nama_pemilik, 100, stdin);
+                    removeNewline(transaksi.pembayaran.kredit.nama_pemilik);
+
+                    printf("Masukkan tanggal kadaluarsa (MM/YY): ");
+                    fgets(transaksi.pembayaran.kredit.tanggal_kadaluarsa, 10, stdin);
+                    removeNewline(transaksi.pembayaran.kredit.tanggal_kadaluarsa);
+                } else if (metode == 2) {
+                    strcpy(transaksi.metode, "Dompet Digital");
+                    printf("Masukkan nama dompet digital: ");
+                    fgets(transaksi.pembayaran.dompet_digital.nama_dompet_digital, 50, stdin);
+                    removeNewline(transaksi.pembayaran.dompet_digital.nama_dompet_digital);
+
+                    printf("Masukkan nomor akun: ");
+                    fgets(transaksi.pembayaran.dompet_digital.nomor_akun, 50, stdin);
+                    removeNewline(transaksi.pembayaran.dompet_digital.nomor_akun);
+                } else if (metode == 3) {
+                    strcpy(transaksi.metode, "Transfer Bank");
+                    printf("Masukkan nama bank: ");
+                    fgets(transaksi.pembayaran.transfer_bank.nama_bank, 50, stdin);
+                    removeNewline(transaksi.pembayaran.transfer_bank.nama_bank);
+
+                    printf("Masukkan nomor rekening: ");
+                    fgets(transaksi.pembayaran.transfer_bank.nomor_rekening, 50, stdin);
+                    removeNewline(transaksi.pembayaran.transfer_bank.nomor_rekening);
+
+                    printf("Masukkan nama pengirim: ");
+                    fgets(transaksi.pembayaran.transfer_bank.nama_pengirim, 100, stdin);
+                    removeNewline(transaksi.pembayaran.transfer_bank.nama_pengirim);
+                } else {
+                    printf("Metode pembayaran tidak tersedia.\n");
+                }
+
+                printf("\nPemabayaran berhasil diproses. Detail transaksi:\n");
+                printf("ID Transaksi: %d\nJudul Buku: %s\nJumlah: %d\nTotal: %.2f\nMetode: %s\n", transaksi.id_transaksi, transaksi.judul, transaksi.jumlah, transaksi.total, transaksi.metode);
             } else {
                 printf("Stok buku %s habis.\n", judul);
             }
@@ -390,6 +545,7 @@ int main() {
     int pilihan;
 
     alokasiMemoriAwal();
+    initQueue();
     addAdmin();
 
     while (1) {
@@ -450,12 +606,51 @@ int main() {
                         {
                             displayBuku();
                             int opsi;
-                            printf("1. Beli buku\n2. Kembali\n");
+                            printf("1. Beli buku\n2. Cari buku\n3. Sortir Buku\n4. Kembali\n");
                             printf("Pilih opsi: ");
                             scanf("%d", &opsi);
                             if (opsi == 1)
                                 beliBuku();
-                            else if (opsi == 2)
+                            else if (opsi == 2) {
+                                while (1) {
+                                    char key[20], value[100];
+                                    int choise;
+                                    printf("\nCari buku berdasarkan:\n1. Judul\n2. Pengarang\n3. Kategori\n4. Kembali\nPilih opsi: ");
+                                    scanf("%d", &choise);
+                                    if (choise == 1) strcpy(key, "judul");
+                                    else if (choise == 2) strcpy(key, "pengarang");
+                                    else if (choise == 3) strcpy(key, "kategori");
+                                    else if (choise == 4) break; 
+                                    else printf("Pilihan tidak valid.\n");
+                                    printf("Masukkan %s yang ingin dicari: ", key);
+                                    getchar();
+                                    fgets(value, 100, stdin);
+                                    removeNewline(value);
+                                    linearSearch(key, value);
+                                }
+                            }     
+                            else if (opsi == 3)
+                                while (1) {
+                                    int choise;
+                                    printf("\nPilih kriteria penyortiran:\n1. Tahun terbit terbaru\n2. Tahun terbit terlama\n3. Harga termurah\n4. Harga termahal\n5. Kembali\nPilih opsi: ");
+                                    scanf("%d", choise);
+                                    if (choise == 1) {
+                                        sortTahunTerbit(1);
+                                        displayBuku();
+                                    } else if (choise == 2) {
+                                        sortTahunTerbit(0);
+                                        displayBuku();
+                                    } else if (choise == 3) {
+                                        sortHarga(1);
+                                        displayBuku();
+                                    } else if (choise == 4) {
+                                        sortHarga(0);
+                                        displayBuku();
+                                    } else if (choise == 5) {
+                                        break;
+                                    } else printf("Pilihan tidak valid.\n");
+                                }
+                            else if (opsi == 4)
                                 break;
                             else
                                 printf("Pilihan tidak valid.\n");
